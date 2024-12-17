bd2df638c6faa044556680b9224177e2ab24cdba
Pablo de la Peña <p@shedcollective.org>
2024-12-17 10:57:15 +0000

    chore: Auto-format

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1941,7 +1941,7 @@ abstract class DefaultController extends Base
                 $oOptionNoExplicit
                     ->setLabel('Explicitly unpublished items')
                     ->setIsQuery(true)
-                    ->setValue('`' . $sColumnIsPublished . '` = 0',);
+                    ->setValue('`' . $sColumnIsPublished . '` = 0');
             }
 
             if ($sColumnDateExpire) {
