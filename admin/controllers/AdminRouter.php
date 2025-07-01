1746cc8acb2d14dbd57b72d1a89b8086448900eb
Pablo de la Peña <p@shedcollective.org>
2025-07-01 10:22:27 +0100

    chore: Remove BaseMiddle
    All functionality can be overridden using the service

--- admin/controllers/AdminRouter.php
+++ admin/controllers/AdminRouter.php
@@ -11,58 +11,30 @@
  */
 
 use Nails\Admin\Constants;
-use Nails\Admin\Exception\RouterException;
+use Nails\Admin\Service\Controller;
+use Nails\Common\Controller\Base;
 use Nails\Common\Exception\FactoryException;
-use Nails\Common\Exception\NailsException;
-use Nails\Components;
 use Nails\Factory;
-
-// --------------------------------------------------------------------------
-
-/**
- * Allow the app to add functionality, if needed
- * Negative conditional helps with static analysis
- */
-if (!class_exists('\App\Admin\Controller\BaseRouter')) {
-    abstract class BaseMiddle extends \Nails\Common\Controller\Base
-    {
-    }
-} else {
-    abstract class BaseMiddle extends \App\Admin\Controller\BaseRouter
-    {
-        public function __construct()
-        {
-            if (!classExtends(parent::class, \Nails\Common\Controller\Base::class)) {
-                throw new NailsException(sprintf(
-                    'Class %s must extend %s',
-                    parent::class,
-                    \Nails\Common\Controller\Base::class
-                ));
-            }
-            parent::__construct();
-        }
-    }
-}
 
 // --------------------------------------------------------------------------
 
 /**
  * Class AdminRouter
  */
-class AdminRouter extends BaseMiddle
+class AdminRouter extends Base
 {
     /**
-     * Initial touch point for admin, all requests are routed through here.
+     * Initial touchpoint for admin, all requests are routed through here.
      *
      * @throws FactoryException
      */
-    public function index()
+    public function index(): void
     {
         if (!isAdmin()) {
             unauthorised();
         }
 
-        /** @var \Nails\Admin\Service\Controller $oControllerService */
+        /** @var Controller $oControllerService */
         $oControllerService = Factory::service('Controller', Constants::MODULE_SLUG);
         $oControllerService->handleRoute();
     }
