be273bdc6cfd8f083d6445f5aa4cfc1ee9c80be4
Pablo de la Peña <p@shedcollective.org>
2025-11-11 15:22:19 +0000

    feat: Compatability for switching from `pre-new-admin branch to `develop`

--- src/Database/Migration/Migration11.php
+++ src/Database/Migration/Migration11.php
@@ -20,5 +20,12 @@ class Migration11 extends Base
     public function execute()
     {
         $this->query('TRUNCATE `{{NAILS_DB_PREFIX}}user_meta_admin`;');
+
+        /**
+         *  Replicating this migration from 10 for applications switching from the `pre-new-admin` branch to `develop`.
+         *  `develop`'s version of 10 will not run for apps previously on `pre-new-admin`, and it is safe to run this
+         *  query more than once
+         */
+        $this->query('UPDATE `{{NAILS_DB_PREFIX}}app_setting` SET `grouping` = "' . Constants::MODULE_SLUG . '" WHERE `grouping` = "admin";');
     }
 }
