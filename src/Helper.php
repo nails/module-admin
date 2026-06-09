96a26477af2e7e31096d9cd3a1135511b4965b8c
Pablo de la Peña <p@shedcollective.org>
2026-06-09 13:51:05 +0100

    chore: Tolerate null value in userCell

--- src/Helper.php
+++ src/Helper.php
@@ -435,13 +435,13 @@ class Helper
     /**
      * Load the admin "user" table cell component
      *
-     * @param Auth\Resource\User|int|string $mUser The user object or the User's ID/email/username
+     * @param null|Auth\Resource\User|int|string $mUser The user object or the User's ID/email/username
      *
      * @throws FactoryException
      * @throws ViewNotFoundException
      * @throws ModelException
      */
-    public static function loadUserCell(Auth\Resource\User|int|string $mUser): string
+    public static function loadUserCell(null|Auth\Resource\User|int|string $mUser): string
     {
         if ($mUser instanceof Auth\Resource\User) {
             $oUser = $mUser;
